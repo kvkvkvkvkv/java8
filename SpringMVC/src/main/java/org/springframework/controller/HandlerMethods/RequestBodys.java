e6cdbf0707af8cca5e00866125432551fa3bcaff
Sreenidhi Kunnavakkam <kvsreenidhi@gmail.com>
2021-03-23 22:20:13 +0530

    @request body

--- SpringMVC/src/main/java/org/springframework/controller/HandlerMethods/RequestBodys.java
+++ SpringMVC/src/main/java/org/springframework/controller/HandlerMethods/RequestBodys.java
@@ -1,11 +1,22 @@
 package org.springframework.controller.HandlerMethods;
 
+import org.springframework.model.Customer;
+import org.springframework.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.logging.Logger;
+
 @Controller
-@RequestMapping("requestBody")
+@RequestMapping("request")
 public class RequestBodys {
 
+    Logger log = Logger.getLogger("RequestBodys");
 
+    @PostMapping(value = "/requestBody", consumes = "application/json", produces = "application/json")
+    public void requestBody(@RequestBody Employee employee){
+        log.info("Customer from request"+employee.toString());
+    }
 }
---
+++ SpringMVC/src/main/java/org/springframework/model/Employee.java
@@ -0,0 +1,9 @@
+package org.springframework.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    String name;
+    String age;
+}
